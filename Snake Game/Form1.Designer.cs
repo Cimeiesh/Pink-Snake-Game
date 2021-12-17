a977a30cde08c0a0b43f5331b31079dd5d875598
Sanna Edström <70255968+Cimeiesh@users.noreply.github.com>
2021-12-17 18:10:48 +0100

    drawSnake, drawFood

--- Snake Game/Form1.Designer.cs
+++ Snake Game/Form1.Designer.cs
@@ -28,23 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Title = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.panel3.SuspendLayout();
@@ -62,57 +65,48 @@
             this.Title.TabIndex = 0;
             this.Title.Text = "PINK SNAKE GAME";
             this.Title.UseMnemonic = false;
-            this.Title.Click += new System.EventHandler(this.label1_Click);
             // 
-            // panel1
+            // gamePanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(15, 256);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 450);
-            this.panel1.TabIndex = 3;
+            this.gamePanel.BackColor = System.Drawing.Color.Black;
+            this.gamePanel.Location = new System.Drawing.Point(15, 256);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(570, 450);
+            this.gamePanel.TabIndex = 3;
             // 
-            // button1
+            // startButton
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.button1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(15, 192);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(280, 58);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "START GAME";
-            this.button1.UseVisualStyleBackColor = false;
+            this.startButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.startButton.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startButton.Location = new System.Drawing.Point(15, 192);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(280, 58);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "START GAME";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
-            // button2
+            // stopButton
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Location = new System.Drawing.Point(305, 192);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(280, 58);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "STOP GAME";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.LightPink;
-            this.label1.Location = new System.Drawing.Point(574, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "MADE BY CIMEIESH";
+            this.stopButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.stopButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.stopButton.FlatAppearance.BorderSize = 0;
+            this.stopButton.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.stopButton.Location = new System.Drawing.Point(305, 192);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(280, 58);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "STOP GAME";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.LightPink;
             this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.nameBox);
             this.panel2.Controls.Add(this.trackBar1);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Location = new System.Drawing.Point(15, 103);
@@ -120,15 +114,32 @@
             this.panel2.Size = new System.Drawing.Size(570, 83);
             this.panel2.TabIndex = 9;
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 87);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "SETTINGS";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(212, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SPEED";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(19, 37);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(175, 20);
+            this.nameBox.TabIndex = 6;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(217, 35);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(350, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 50;
             // 
             // label4
             // 
@@ -140,22 +151,15 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "NAME";
             // 
-            // trackBar1
+            // label2
             // 
-            this.trackBar1.Location = new System.Drawing.Point(183, 35);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(384, 45);
-            this.trackBar1.TabIndex = 5;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.trackBar1.Value = 50;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(19, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(129, 20);
-            this.textBox1.TabIndex = 6;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SETTINGS";
             // 
             // panel3
             // 
@@ -167,24 +171,6 @@
             this.panel3.Size = new System.Drawing.Size(282, 513);
             this.panel3.TabIndex = 10;
             // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.OldLace;
-            this.panel4.Location = new System.Drawing.Point(19, 49);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(245, 444);
-            this.panel4.TabIndex = 0;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(190, 10);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 25);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "SPEED";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -195,35 +181,57 @@
             this.label7.TabIndex = 8;
             this.label7.Text = "HISTORY";
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.OldLace;
+            this.panel4.Location = new System.Drawing.Point(19, 49);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(245, 444);
+            this.panel4.TabIndex = 0;
+            // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.Pink;
-            this.panel5.Controls.Add(this.label6);
+            this.panel5.Controls.Add(this.scoreLabel);
             this.panel5.Controls.Add(this.label5);
             this.panel5.Location = new System.Drawing.Point(594, 103);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(282, 83);
             this.panel5.TabIndex = 9;
             // 
-            // label6
+            // scoreLabel
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(146, 17);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 51);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "0";
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(170, 17);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(44, 51);
+            this.scoreLabel.TabIndex = 13;
+            this.scoreLabel.Text = "0";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(22, 20);
+            this.label5.Location = new System.Drawing.Point(46, 20);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(128, 44);
             this.label5.TabIndex = 12;
             this.label5.Text = "SCORE:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightPink;
+            this.label1.Location = new System.Drawing.Point(573, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "MADE BY CIMEIESH";
             // 
             // Form1
             // 
@@ -231,14 +239,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(890, 719);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.gamePanel);
             this.Controls.Add(this.Title);
             this.Name = "Form1";
             this.Text = "Snake Game";
@@ -257,13 +265,12 @@
         #endregion
 
         private System.Windows.Forms.Label Title;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox nameBox;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
@@ -271,8 +278,11 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Snake Game/Form1.cs
+++ Snake Game/Form1.cs
@@ -12,18 +12,104 @@ namespace Snake_Game
 {
     public partial class Form1 : Form
     {
+
+        // ========= My Snake Default =========
+        PictureBox[] snakeParts;
+        int snakeSize = 5;
+        Point location = new Point(120, 120);
+        string direction = "Right";
+        bool changingDirection = false;
+
+        //========= My Food Default =========
+        PictureBox food = new PictureBox();
+        Point foodLocation = new Point(0, 0);
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //========= Cleaning The Game For A New Game =========
+            gamePanel.Controls.Clear();
+            snakeParts = null;
+            scoreLabel.Text = "0";
+            snakeSize = 5;
+            direction = "Right";
+            location = new Point(120, 120);
+
+            //========= Start My Game =========
+            drawSnake();
+            drawFood();
+
+            timer1.Start();
+
+            //========= Disable Setting and Start =========
+            trackBar1.Enabled = false;
+            startButton.Enabled = false;
+            nameBox.Enabled = false;
+
+            //========= Enable Stop Button =========
+            stopButton.Enabled = true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void drawSnake()
         {
+            snakeParts = new PictureBox[snakeSize];
+
+            // ========= A Loop For Increasing The Snake =========
+
+            for (int i = 0; i < snakeSize; i++)
+            {
+                snakeParts[i] = new PictureBox();
+                snakeParts[i].Size = new Size(15, 15);
+                snakeParts[i].BackColor = Color.HotPink;
+                snakeParts[i].BorderStyle = BorderStyle.FixedSingle;
+                snakeParts[i].Location = new Point(location.X-(15*i), location.Y);
+                gamePanel.Controls.Add(snakeParts[i]);
+            }
+        }
+
+        private void drawFood()
+        {
+            Random rnd = new Random();
+            int Xrand = rnd.Next(38) * 15;
+            int Yrand = rnd.Next(30) * 15;
+
+            bool snakeEat = true;
+
+            //========= Check If Snake Eats The Food =========
+            while (snakeEat)
+            {
+                for(int i = 0;i < snakeSize; i++)
+                {
+                    if(snakeParts[i].Location == new Point(Xrand, Yrand))
+                    {
+                        Xrand = rnd.Next(38) * 15;
+                        Yrand = rnd.Next(30) * 15;
+                    }
+                    else
+                    {
+                        snakeEat = false;
+                    }
+                }
+            }
+
+            if (snakeEat == false)
+            {
+                foodLocation = new Point(Xrand, Yrand);
+                food.Size = new Size(15, 15);
+                food.BackColor = Color.LimeGreen;
+                food.BorderStyle = BorderStyle.FixedSingle;
+                food.Location = foodLocation;
+                gamePanel.Controls.Add(food);
+            }
 
         }
     }
